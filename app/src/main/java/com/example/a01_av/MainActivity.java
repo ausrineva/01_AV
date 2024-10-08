372f606c6acc849643855ad799cfd6e876f7faf1
ausrineva <159789891+ausrineva@users.noreply.github.com>
2024-10-08 14:42:39 +0300

    Antrinis išsaugojimas į git. Pagal užduotį, įkeliami du elementa: Text View ir Button. Pridedamas kodo scriptas Main Activity, kad kai paspaudžiamas mygtukas, atsiranda tekstas "Sveiki visi!" (Teksto elementas yra paliktas tuščias, kad atlikus šią funkciją, pasirodytų naujas tekstas).

--- app/src/main/java/com/example/a01_av/MainActivity.java
+++ app/src/main/java/com/example/a01_av/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.a01_av;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,34 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tekstas;
+    Button pirmutinismygtukas;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        tekstas = findViewById(R.id.tekstas);
+        pirmutinismygtukas = findViewById(R.id.pirmutinismygtukas);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+//funkcija, su kuria paspaudus ant mygtuko atsiranda tekstas
+        pirmutinismygtukas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                tekstas.setText("Sveiki atvykę!");
+            }
+        });
+
+ //funkcija su kuria atsidanda antras mygtukas
+
+
     }
 }
